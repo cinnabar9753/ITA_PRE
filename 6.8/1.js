d89a4604f5128a11b972a4e9fe56446ae902a16d
cinnabar <1225715474@qq.com>
2022-06-09 18:31:02 +0800

    first commit

---
+++ 6.8/1.js
@@ -0,0 +1,13 @@
+var user = {}
+user.name = 'John'
+user.surname = 'Mike'
+user.name = 'Peter'
+user.name = null
+
+
+var fruit = {
+    apple: 20,
+    pear: 20,
+    peach: 10
+};
+console.log(fruit.apple+fruit.peach+fruit.pear);
